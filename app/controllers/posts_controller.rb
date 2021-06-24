bf936ea52cfedc8f9e4e08c77c69c304ed4fd462
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-06-23 22:43:40 -0400

    commit tree 8359d1cd3348bf3b457c0b315d7d41a2d15e3f2b

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,12 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+   
   end
 
   def create
     post = Post.create(post_params)
+    
     redirect_to post
   end
 
